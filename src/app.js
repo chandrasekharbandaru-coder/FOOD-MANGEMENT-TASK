90f9e3231bad3122fc4c9f7b6960db577e7557d1
durga chandra shekhar <chandrasekhar.bandaru@hiringhood.com>
2025-12-31 23:16:51 +0530

    Update app.js configuration

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ connectDB();
 
 app.use("/api/auth", require("./routes/auth.routes"));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
