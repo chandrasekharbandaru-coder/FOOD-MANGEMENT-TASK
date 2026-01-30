5f84d31fff1d67a285caad7feae2320937da7f35
durga chandra shekhar <chandrasekhar.bandaru@hiringhood.com>
2026-01-30 17:35:29 +0530

    Add Cashfree utility configuration

---
+++ src/utils/cashfree.js
@@ -0,0 +1,19 @@
+const axios = require("axios");
+
+const CASHFREE_BASE_URL =
+  process.env.CASHFREE_ENV === "production"
+    ? "https://api.cashfree.com/pg"
+    : "https://sandbox.cashfree.com/pg";
+
+const cashfree = axios.create({
+  baseURL: CASHFREE_BASE_URL,
+  headers: {
+    "x-client-id": process.env.CASHFREE_APP_ID,
+    "x-client-secret": process.env.CASHFREE_SECRET_KEY,
+    "x-api-version": "2023-08-01",
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
+});
+
+module.exports = cashfree;
